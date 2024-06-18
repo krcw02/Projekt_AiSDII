eada7c1a748637fbe9b1b0c0680e9f56de50b822
Maciej Zajdel <127329397+MaciekZ23@users.noreply.github.com>
2024-06-18 23:27:32 +0200

    Testowanie algorytmu KMP

---
+++ IMPLEMENTACJA_ALGORYTMÓW/module.js
@@ -0,0 +1,58 @@
+// module.js
+
+// FUNKCJA POMOCNICZA DO GENEROWANIA TABLICY PRZEJSC (prefix function) DLA WZORCA
+function obliczanieTablicyPrzejsc(wzorzec) {
+    const M = wzorzec.length;
+    const lps = new Array(M).fill(0);
+    let dlugosc = 0; // dlugosc poprzedniego najdluzszego prefiksu-sufiksu
+    let i = 1;
+
+    while(i < M) {
+        if(wzorzec[i] === wzorzec[dlugosc]) {
+            dlugosc++;
+            lps[i] = dlugosc;
+            i++;
+        } else {
+            if(dlugosc !== 0) {
+                dlugosc = lps[dlugosc - 1];
+            } else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+    return lps;
+}
+
+// FUNKCJA ZAMIENIAJACA WZORZEC W TEKSCIE
+function zamienWzorzec(tekst, staryWzorzec, nowyWzorzec) {
+    const N = tekst.length;
+    const M = staryWzorzec.length;
+    const lps = obliczanieTablicyPrzejsc(staryWzorzec);
+    let i = 0; // Indeks dla tekstu
+    let j = 0; // Indeks dla wzorca
+    let wynik = '';
+    let poczatek = 0; // Poczatkowa pozycja fragmentu do skopiowania
+    while(i < N) {
+        if(staryWzorzec[j] === tekst[i]) {
+            i++;
+            j++;
+        }
+        if(j === M) {
+            wynik += tekst.substring(poczatek, i - M) + nowyWzorzec;
+            poczatek = i;
+            j = 0;
+        } else if(i < N && staryWzorzec[j] !== tekst[i]) {
+            if(j !== 0) {
+                j = lps[j - 1];
+            } else {
+                i++;
+            }
+        }
+    }
+    wynik += tekst.substring(poczatek);
+    return wynik;
+}
+
+
+module.exports = { zamienWzorzec };
---
+++ IMPLEMENTACJA_ALGORYTMÓW/test_module.js
@@ -0,0 +1,62 @@
+const { zamienWzorzec } = require('./module');
+
+function assertEqual(actual, expected, message) {
+    if (actual === expected) {
+        console.log(`✔️  ${message}`);
+    } else {
+        console.error(`❌  ${message}`);
+        console.error(`   Oczekiwane: ${expected}, ale otrzymane: ${actual}`);
+    }
+}
+
+function runTests() {
+    console.log("Uruchamianie testów dla zamienWzorzec...");
+
+    const tekstMelodii = "ZapoliSzKryspoli123polipoliabcdUlalapolipoliboli";
+    const staryWzorzec = "poli";
+    const nowyWzorzec = "boli";
+    const oczekiwanyTekstMelodii = "ZaboliSzKrysboli123boliboliabcdUlalaboliboliboli";
+    assertEqual(zamienWzorzec(tekstMelodii, staryWzorzec, nowyWzorzec), oczekiwanyTekstMelodii, "zamienWzorzec powinno poprawnie zamienić 'poli' na 'boli'");
+
+    const tekst1 = "polishBookspolimamagotujepoliposluchammuzyki";
+    const oczekiwanyTekst1 = "bolishBooksbolimamagotujeboliposluchammuzyki";
+    assertEqual(zamienWzorzec(tekst1, staryWzorzec, nowyWzorzec), oczekiwanyTekst1, "zamienWzorzec powinno poprawnie zamienić 'poli' na 'boli'");
+
+    const tekst4 = "polipoli";
+    const oczekiwanyTekst4 = "boliboli";
+    assertEqual(zamienWzorzec(tekst4, staryWzorzec, nowyWzorzec), oczekiwanyTekst4, "zamienWzorzec powinno poprawnie zamienić 'poli' na 'boli'");
+
+    const tekst5 = "polipoliSzkolapolipoli";
+    const oczekiwanyTekst5 = "boliboliSzkolaboliboli";
+    assertEqual(zamienWzorzec(tekst5, staryWzorzec, nowyWzorzec), oczekiwanyTekst5, "zamienWzorzec powinno poprawnie zamienić 'poli' na 'boli'");
+
+    const tekst6 = "XpolipoliYpolipoliZ";
+    const oczekiwanyTekst6 = "XboliboliYboliboliZ";
+    assertEqual(zamienWzorzec(tekst6, staryWzorzec, nowyWzorzec), oczekiwanyTekst6, "zamienWzorzec powinno poprawnie zamienić 'poli' na 'boli'");
+
+    const tekst2 = "helloworld";
+    const staryWzorzec2 = "world";
+    const nowyWzorzec2 = "universe";
+    const oczekiwanyTekst2 = "hellouniverse";
+    assertEqual(zamienWzorzec(tekst2, staryWzorzec2, nowyWzorzec2), oczekiwanyTekst2, "zamienWzorzec powinno poprawnie zamienić 'world' na 'universe'");
+
+    const tekst3 = "abcdabcd";
+    const staryWzorzec3 = "abc";
+    const nowyWzorzec3 = "xyz";
+    const oczekiwanyTekst3 = "xyzdxyzd";
+    assertEqual(zamienWzorzec(tekst3, staryWzorzec3, nowyWzorzec3), oczekiwanyTekst3, "zamienWzorzec powinno poprawnie zamienić 'abc' na 'xyz'");
+
+    const tekst7 = "abcabcabc";
+    const staryWzorzec7 = "abc";
+    const nowyWzorzec7 = "123";
+    const oczekiwanyTekst7 = "123123123";
+    assertEqual(zamienWzorzec(tekst7, staryWzorzec7, nowyWzorzec7), oczekiwanyTekst7, "zamienWzorzec powinno poprawnie zamienić 'abc' na '123'");
+
+    const tekst8 = "hellohellohello";
+    const staryWzorzec8 = "hello";
+    const nowyWzorzec8 = "hi";
+    const oczekiwanyTekst8 = "hihihi";
+    assertEqual(zamienWzorzec(tekst8, staryWzorzec8, nowyWzorzec8), oczekiwanyTekst8, "zamienWzorzec powinno poprawnie zamienić 'hello' na 'hi'");
+}
+
+runTests();
